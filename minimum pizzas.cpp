841c49b58e01412aa4c02e3e7e1d51589a78103d
Muskan Dubey <muskandubey184@outlook.com>
2024-12-10 11:21:01 +0530

    Create minimum pizzas.cpp

---
+++ minimum pizzas.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    int n,x;
+    
+    while(t--){
+        cin>>n>>x;
+        int pizzas_needed= (n*x+3)/4;
+        cout<<pizzas_needed<<endl;;
+
+
+    }
+    return 0;
+}
