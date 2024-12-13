732832f77dc51fd673137c075fcd514d02d40770
Muskan Dubey <muskandubey184@outlook.com>
2024-12-13 12:00:15 +0530

    Create Reach on Time.cpp

---
+++ Reach on Time.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t,o;
+    cin>>t;
+    while(t--){
+        cin>>o;
+        if(o>=30){
+            cout<<"yes"<<endl;
+        }else{
+            cout<<"no"<<endl;
+        }
+    }
+    return 0;
+}
