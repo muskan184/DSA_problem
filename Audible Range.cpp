9fd47e1905c99c09e9a8b71cc5eb3b1d56d62f2c
Muskan Dubey <muskandubey184@outlook.com>
2024-12-13 11:57:14 +0530

    Create Audible Range.cpp

---
+++ Audible Range.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+     int t,s;
+     cin>> t;
+     while(t--){
+         cin>>s;
+         if(s>=67 && s<=45000){
+             cout<<"yes"<<endl;
+         }else{
+             cout<<"no"<<endl;
+         }
+     }
+     return 0;
+}
