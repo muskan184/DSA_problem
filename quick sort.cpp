bfb88e114d9dc2e7a811f26f36f86a362c3217f7
Muskan Dubey <muskandubey184@outlook.com>
2024-12-16 00:59:50 +0530

    Create quick sort.cpp

---
+++ quick sort.cpp
@@ -0,0 +1,32 @@
+  void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low<high){
+            int pi=partition(arr,low,high);
+            quickSort(arr,low,pi-1);
+            quickSort(arr,pi+1,high);
+        }
+    }
+
+  public:
+    // Function that takes last element as pivot, places the pivot element at
+    // its correct position in sorted array, and places all smaller elements
+    // to left of pivot and all greater elements to right of pivot.
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+            int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+    }
