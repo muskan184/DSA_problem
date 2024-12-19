c992c2b6f341c064b37a6e5b850d9b2f2f7718ab
Muskan Dubey <muskandubey184@outlook.com>
2024-12-19 08:05:34 +0530

    Create ai is coming.cpp

---
+++ ai is coming.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int dificulty;
+    cin>>dificulty;
+    
+    if(dificulty<=60){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
