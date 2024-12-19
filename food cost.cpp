2f4efd2d882eba68dc7ab8fcffefb05e397d21c1
Muskan Dubey <muskandubey184@outlook.com>
2024-12-19 08:06:08 +0530

    Create food cost.cpp

---
+++ food cost.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X,Y;
+    cin>>X>>Y;
+    int x=X*6;
+    std::cout << x+Y << std::endl;
+    
+    return 0;
+}
