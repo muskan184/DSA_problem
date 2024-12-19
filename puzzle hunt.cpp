2c0c4f19a3c6fac4b4e0e00ba1a168c1abd110d9
Muskan Dubey <muskandubey184@outlook.com>
2024-12-20 01:23:47 +0530

    Create puzzle hunt.cpp

---
+++ puzzle hunt.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int n ;
+ cin>> n;
+ if(n>=6 && n<=8){
+     std::cout << "yes" << std::endl;
+ }else{
+     cout<<"no"<<endl;
+ }
+ return 0;
+}
