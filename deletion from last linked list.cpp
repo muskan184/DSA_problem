bbcf244049228e9587dd666be2d3d1e0794cf06d
Muskan Dubey <muskandubey184@outlook.com>
2024-12-25 08:21:43 +0530

    Create deletion from last linked list.cpp

---
+++ deletion from last linked list.cpp
@@ -0,0 +1,14 @@
+    Node *insertAtEnd(Node *head, int x) {
+  if(head == NULL){
+      return new Node(x);
+  }
+  Node* temp=head;
+  while(temp->next!=NULL){
+      temp=temp->next;
+  }
+  Node* newNode=new Node(x);
+  temp->next=newNode;
+  return head;
+       
+        
+    }
