1d869355ce7065ce83491e10dfcee0ceea70ce56
Muskan Dubey <muskandubey184@outlook.com>
2024-12-27 17:35:16 +0530

    Create Merry Christmas!.cpp

---
+++ Merry Christmas!.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int time;
+ cin>>time;
+ if(time<3 ){
+     cout<<1<<endl;
+ }else if(time>2 && time<7){
+     cout<<2<<endl;
+ }else{
+     cout<<3<<endl;
+ }
+ return 0;
+}
