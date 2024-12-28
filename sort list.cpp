bdffbe24ea2de39d0a8412a37355312b18c37d7f
Muskan Dubey <muskandubey184@outlook.com>
2024-12-28 09:48:50 +0530

    Create sort list.cpp

---
+++ sort list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+    ListNode* findMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (prev) prev->next = nullptr; 
+        return slow;
+    }
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* mid = findMiddle(head);       
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return mergeTwoLists(left, right);
+    }
+};
