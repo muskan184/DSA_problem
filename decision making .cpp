948ff0ce6f3453b89a2ac1d32a9cf1d29fe0128b
Muskan Dubey <muskandubey184@outlook.com>
2025-01-13 09:50:27 +0530

    Create decision making .cpp

---
+++ decision making .cpp
@@ -0,0 +1,10 @@
+   string compareNM(int n, int m){
+        // code here
+        if(n>m){
+            return "greater";
+        }else if(n<m){
+            return "lesser";
+        }else
+            return "equal";
+        
+    }
