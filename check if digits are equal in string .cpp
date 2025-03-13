c38e4fcb2e455d27133d1b177d2b99e52506daae
Muskan Dubey <muskandubey184@outlook.com>
2025-03-13 14:34:52 +0530

    Create check if digits are equal in string .cpp

---
+++ check if digits are equal in string .cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.size() > 2) {  
+            string k = "";
+            for (int i = 0; i < s.size() - 1; i++) { 
+                int sum = (s[i] - '0' + s[i + 1] - '0') % 10;
+                k += to_string(sum);
+            }
+            s = k;  
+        }
+        return s[0] == s[1];  
+    }
+};
