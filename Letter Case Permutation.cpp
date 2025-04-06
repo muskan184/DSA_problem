5c5d30b9f01e9bb5d26f094100e58ecd43fac185
Muskan Dubey <muskandubey184@outlook.com>
2025-04-06 22:44:36 +0530

    Create Letter Case Permutation.cpp

---
+++ Letter Case Permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    void solve(string ip,string op,vector<string>&ans){
+    if(ip.length()==0){
+        ans.push_back(op);
+        return;
+    }
+    if(isalpha(ip[0]) ){
+        string op1=op;
+        string op2=op;
+        op1.push_back(tolower(ip[0]));
+        op2.push_back(toupper(ip[0]));
+        ip.erase(ip.begin()+0);
+        solve(ip,op1,ans);
+        solve(ip,op2,ans);
+    }else{
+        string op1=op;
+        op1.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        solve(ip,op1,ans);
+    }
+}
+public:
+vector<string> letterCasePermutation(string s) {
+    string ip=s;
+    string op="";
+    vector<string> ans;
+    solve(ip,op,ans);
+    return ans;
+    }
+};
