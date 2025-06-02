70e0c3f4e545b5974c0cac1e78fd09a40954845d
Muskan Dubey <muskandubey184@outlook.com>
2025-06-02 23:21:09 +0530

    Create Recover Binary Search Tree.cpp

---
+++ Recover Binary Search Tree.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+    TreeNode* prev = nullptr;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+
+        if (prev && root->val < prev->val) {
+            if (!first) {
+                first = prev;
+            }
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        if (first && second) {
+            swap(first->val, second->val);
+        }
+    }
+};
