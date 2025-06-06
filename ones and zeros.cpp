e33167b9b240b580a02bae2f95410a42fb8568ab
Muskan Dubey <muskandubey184@outlook.com>
2025-06-06 19:19:46 +0530

    Create ones and zeros.cpp

---
+++ ones and zeros.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+pair<int,int>countZeroOne(string &s){
+    int zero=0,one=0;
+    for(char c:s){
+        if(c=='0')zero++;
+        else one++;
+
+   
+    }
+         return{zero,one};
+}
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+        for(string &s:strs){
+            auto [zero,one]=countZeroOne(s);
+
+            for(int i=m;i>=zero;i--){
+                for(int j=n;j>=one;j--){
+                    dp[i][j]=max(dp[i][j],1+dp[i-zero][j-one]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
