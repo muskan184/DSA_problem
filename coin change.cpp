02e53202cfe88599712a8011dcbcdcb5a603b8de
Muskan Dubey <muskandubey184@outlook.com>
2026-01-03 15:20:47 +0530

    Implement coin change solution using dynamic programming

---
+++ coin change.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+
+        if (dp[amount] == INT_MAX) return -1;
+        return dp[amount]; 
+    }
+};
